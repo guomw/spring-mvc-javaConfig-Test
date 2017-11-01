0dff30c1bfb419d4a94e42bc9a32c26823f30282
guomw <516985724@163.com>
2017-11-01 20:05:15 +0800

    修改

--- src/main/java/com/guomw/mvc/config/ApplicationInitializer.java
+++ src/main/java/com/guomw/mvc/config/ApplicationInitializer.java
@@ -2,8 +2,14 @@ package com.guomw.mvc.config;
 
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
+import javax.servlet.Filter;
+
 public class ApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer{
 
+    /**
+     * 配置dispatcher servlet，如果在root config指定了该转发规则就可以忽略
+     * @return
+     */
     @Override
     protected Class<?>[] getRootConfigClasses() {
         return new Class<?>[]{MvcConfig.class};
@@ -15,13 +21,21 @@ public class ApplicationInitializer extends AbstractAnnotationConfigDispatcherSe
     }
 
 
+    /**
+     * 指定开始被servlet处理的url,配置从/开始
+     * @return
+     */
     @Override
     protected String[] getServletMappings() {
         return new String[]{"/"};
     }
 
+    /**
+     * 配置servlet过滤器
+     * @return
+     */
     @Override
-    protected javax.servlet.Filter[] getServletFilters() {
+    protected Filter[] getServletFilters() {
         return super.getServletFilters();
     }
 }
--- src/main/java/com/guomw/mvc/config/MvcConfig.java
+++ src/main/java/com/guomw/mvc/config/MvcConfig.java
@@ -40,65 +40,84 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
 
     /**
      * 静态资源设置
+     *
      * @param registry
      */
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         super.addResourceHandlers(registry);
-        for (String resourcePath:STATIC_RESOURCE_PATH) {
-            registry.addResourceHandler("/"+resourcePath+"/**").addResourceLocations("/"+resourcePath+"/");
+        for (String resourcePath : STATIC_RESOURCE_PATH) {
+            registry.addResourceHandler("/" + resourcePath + "/**").addResourceLocations("/" + resourcePath + "/");
 
         }
     }
 
     /**
      * 设置html视图解析器
+     *
      * @param registry
      */
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
-        //super.configureViewResolvers(registry);
-        registry.viewResolver(thymeleafViewResolver());
+        registry.viewResolver(viewResolver());
     }
 
     /**
      * 设置异常处理
+     *
      * @param exceptionResolvers
      */
     @Override
     public void configureHandlerExceptionResolvers(List<HandlerExceptionResolver> exceptionResolvers) {
         super.configureHandlerExceptionResolvers(exceptionResolvers);
-       // exceptionResolvers.add(new WEb)
+        // exceptionResolvers.add(new WEb)
     }
 
     /**
-     * thymeleaf 模版解析器
+     * ThymeleafViewResolver 页面解析器
+     *
      * @return
      */
-    private ViewResolver thymeleafViewResolver(){
-
-        SpringResourceTemplateResolver templateResolver=new SpringResourceTemplateResolver();
-        templateResolver.setPrefix("/views/");
-        templateResolver.setSuffix(".html");
-        templateResolver.setApplicationContext(webApplicationContext);
-        templateResolver.setCharacterEncoding("utf-8");
-
-        if (environment.acceptsProfiles("development"))
-        {
-            templateResolver.setCacheable(false);
-        }
-
-        SpringTemplateEngine engine=new SpringTemplateEngine();
-        engine.setTemplateResolver(templateResolver);
-
-        ThymeleafViewResolver resolver=new ThymeleafViewResolver();
-        resolver.setTemplateEngine(engine);
+    private ViewResolver viewResolver() {
+        ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+        resolver.setTemplateEngine(templateEngine());
         resolver.setContentType("text/html;charset=utf-8");
         resolver.setCharacterEncoding("utf-8");
         resolver.setOrder(1);
         return resolver;
 
     }
+
+
+    /**
+     * spring的模版引擎
+     *
+     * @return
+     */
+    private SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine engine = new SpringTemplateEngine();
+        engine.setTemplateResolver(templateResolver());
+        return engine;
+    }
+
+    /**
+     * 模版解析器
+     *
+     * @return
+     */
+    private SpringResourceTemplateResolver templateResolver() {
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setPrefix("/views/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setApplicationContext(webApplicationContext);
+        templateResolver.setCharacterEncoding("utf-8");
+
+        if (environment.acceptsProfiles("development")) {
+            templateResolver.setCacheable(false);
+        }
+        return templateResolver;
+    }
+
 
 
 
